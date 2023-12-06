1a25dde5ec861f0a02698714def9f20f6ac63da9
Shawn Johnson <11654145+brthurr@users.noreply.github.com>
2023-12-06 12:37:57 -0600

    models, config, and db migrations

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
-from models import db, Owner, Team, Game
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+from models import db
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config.from_object('config.config.Config')
+print(app.config['SQLALCHEMY_DATABASE_URI'])
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World'
+    return 'Hello World'
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
