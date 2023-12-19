f3632d29e7d0faf901424f74b0d7af02626c50fa
Shawn Johnson <11654145+brthurr@users.noreply.github.com>
2023-12-19 11:15:52 -0600

    modified schedule to update_tournament every Tuesday at 3 AM

--- helpers/scheduler.py
+++ helpers/scheduler.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
+from apscheduler.triggers.cron import CronTrigger
 import subprocess
 
 scheduler = BackgroundScheduler(daemon=True)
@@ -30,8 +31,33 @@ def update_game_results_job():
         print(f"Error running Flask command: {e}")
 
 
+def update_tournament_command():
+    try:
+        current_year = datetime.now().year
+        command = [
+            "flask",
+            "update_tournament",
+            "--start-week",
+            "15",
+            "--end-week",
+            "17",
+            f"--year={current_year}",
+        ]
+        subprocess.run(command, check=True)
+    except Exception as e:
+        print(f"Error running Flask command: {e}")
+
+
+# Schedule for update_game_results_job
 scheduler.add_job(
     update_game_results_job,
     trigger=IntervalTrigger(minutes=5),
 )
+
+# Schedule for update_tournament_command
+scheduler.add_job(
+    update_tournament_command,
+    trigger=CronTrigger(day_of_week="tue", hour=3, minute=0),
+)
+
 scheduler.start()
